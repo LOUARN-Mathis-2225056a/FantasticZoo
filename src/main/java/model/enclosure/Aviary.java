e4d9e3cfe17e390b588e47920da8f70f8b0a6c3b
LOUARN-Mathis-2225056a <mathis.louarn@etu.univ-amu.fr>
2023-11-08 23:44:24 +0100

    Added Aviary constructor

--- src/main/java/model/enclosure/Aviary.java
+++ src/main/java/model/enclosure/Aviary.java
@@ -3,6 +3,11 @@ package model.enclosure;
 public class Aviary extends Enclosure{
     private float height;
 
+    public Aviary(float height){
+        super();
+        this.height = height;
+    }
+
     @Override
     public void clean(){
         /*code*/
