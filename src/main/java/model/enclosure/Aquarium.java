db5992799b8eea1917e266dbb0150a251063e735
LOUARN-Mathis-2225056a <mathis.louarn@etu.univ-amu.fr>
2023-11-08 23:43:37 +0100

    Added Aquarium constructor

--- src/main/java/model/enclosure/Aquarium.java
+++ src/main/java/model/enclosure/Aquarium.java
@@ -4,6 +4,12 @@ public class Aquarium extends Enclosure{
     private float depht;
     private int salinity;
 
+    public Aquarium(float depht, int salinity){
+        super();
+        this.depht = depht;
+        this.salinity = salinity;
+    }
+
     @Override
     public void clean(){
         /*code*/
