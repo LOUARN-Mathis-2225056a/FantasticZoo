4e15506fe9018289877050aff17bfeeb9f9502fb
LeoG <103144119+LeoG1904@users.noreply.github.com>
2023-11-09 11:08:26 +0100

    j'avais oublié un constructeur

--- src/main/java/model/creature/Lycanthrope.java
+++ src/main/java/model/creature/Lycanthrope.java
@@ -2,6 +2,10 @@ package model.creature;
 
 public class Lycanthrope extends Viviparous implements Runner{
 
+    public Lycanthrope(String name, boolean sexe, float weight, float height, int hunger, boolean sleep, int health, int age) {
+        super(name, sexe, weight, height, hunger, sleep, health, age);
+    }
+
     @Override
     public void creatureRun() {
 
