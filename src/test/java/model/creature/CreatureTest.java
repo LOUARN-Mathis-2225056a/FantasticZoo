3113e96989eeb150c0f201fa338d37ca5077f774
LeoG <103144119+LeoG1904@users.noreply.github.com>
2023-11-09 09:54:40 +0100

    création de la CreatureTest

---
+++ src/test/java/model/creature/CreatureTest.java
@@ -0,0 +1,42 @@
+package model.creature;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreatureTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void eat() {
+    }
+
+    @Test
+    void emitSound() {
+    }
+
+    @Test
+    void heal() {
+    }
+
+    @Test
+    void wake() {
+    }
+
+    @Test
+    void sleep() {
+    }
+
+    @Test
+    void gettingOld() {
+    }
+}
