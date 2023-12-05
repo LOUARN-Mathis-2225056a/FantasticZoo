d1afc6e7fe8d4ad314363c89a66baf1cf2354579
LeoG <103144119+LeoG1904@users.noreply.github.com>
2023-12-05 14:15:00 +0100

    Ajout de l'ensemble des caractéristique d'un Lycanthrope

--- src/main/java/td2/model/lycan/Lycanthrope2.java
+++ src/main/java/td2/model/lycan/Lycanthrope2.java
@@ -1,4 +1,16 @@
 package td2.model.lycan;
 
 public class Lycanthrope2 {
+    private boolean sex;
+
+    public enum AgeCategory {
+        YOUNG,
+        ADULT,
+        OLD;
+    }
+    private AgeCategory age;
+    private int strength;
+    private int domination; // corresponding to the difference between the domination exercised and those suffered
+    private int level; // corresponding to the subjective quality criterion of a lycanthrope, both male and female; calculated based on age category, strength, dominance factor and rank
+    private int impetuosity;
 }
