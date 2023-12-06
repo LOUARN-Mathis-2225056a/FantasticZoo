14153ddbc3595356cfcf1937f55ab700d446d1a7
LeoG <103144119+LeoG1904@users.noreply.github.com>
2023-12-06 22:47:07 +0100

    dorénavant si il y a 2 LycanthropeSolitary (male et female), la colonie créer un nouvelle enclos avec une nouvelle colonie pour les placer dedans ou les déplace dans un enclos avec une meute vide

--- src/main/java/td2/controller/LycanthropeColony.java
+++ src/main/java/td2/controller/LycanthropeColony.java
@@ -13,8 +13,7 @@ import td2.model.roar.Submission;
 
 import java.util.ArrayList;
 
-public class LycanthropeColony {
-    private ArrayList<Enclosure2> listEnclosure= new ArrayList<Enclosure2>();
+public class LycanthropeColony implements Runnable{
     static public void main(String[] arg){
 
         // colony creation
@@ -22,12 +21,17 @@ public class LycanthropeColony {
 
         // enclosure creation
         Enclosure2 encloOne = new Enclosure2();
+        Enclosure2 encloTwo = new Enclosure2();
+        Enclosure2 encloTree = new Enclosure2();
 
         // enclosure in colony
         lycanthropeColony.addEnclosure(encloOne);
+        lycanthropeColony.addEnclosure(encloTwo);
+        lycanthropeColony.addEnclosure(encloTree);
 
         // LycanthropeSolitary creation
         Lycanthrope2 lycanOne = new LycanthropeSolitary(true, Lycanthrope2.AgeCategory.YOUNG,0,0,0,0,encloOne);
+        Lycanthrope2 lycanOne2 = new LycanthropeSolitary(false, Lycanthrope2.AgeCategory.YOUNG,0,0,0,0,encloTwo);
 
         // LycanthropeInPack creation
 
@@ -36,20 +40,80 @@ public class LycanthropeColony {
         LycanthropeInPack lycan4 = new LycanthropeInPack(false, Lycanthrope2.AgeCategory.YOUNG,0,0,0,0,5,encloOne.getLycanPack());
         LycanthropeInPack lycan5 = new LycanthropeInPack(false, Lycanthrope2.AgeCategory.YOUNG,0,0,0,0,2,encloOne.getLycanPack());
 
+
+        LycanthropeInPack lycan6 = new LycanthropeInPack(true, Lycanthrope2.AgeCategory.YOUNG,0,0,0,0,2,encloTwo.getLycanPack());
+        LycanthropeInPack lycan7 = new LycanthropeInPack(false, Lycanthrope2.AgeCategory.YOUNG,0,0,0,0,2,encloTwo.getLycanPack());
+        LycanthropeInPack lycan8 = new LycanthropeInPack(false, Lycanthrope2.AgeCategory.YOUNG,0,0,0,0,2,encloTwo.getLycanPack());
+        LycanthropeInPack lycan9 = new LycanthropeInPack(true, Lycanthrope2.AgeCategory.YOUNG,0,0,0,0,2,encloTwo.getLycanPack());
+
         // test zone
         AlphaCouple coupleOne = new AlphaCouple(lycanTree,lycanTwo);
-        encloOne.getLycanPack().setAlphaCouple(coupleOne);
+        /*encloOne.getLycanPack().setAlphaCouple(coupleOne);
         coupleOne.giveBirth();
         coupleOne.giveBirth();
         encloOne.getLycanPack().showToString();
-        lycanthropeColony.showAllLycan();
+        lycanthropeColony.showAllLycan();*/
     }
-
+    private ArrayList<Enclosure2> listEnclosure= new ArrayList<Enclosure2>();
+    private Thread thread;
+    public LycanthropeColony(){
+        thread = new Thread(this);
+        thread.start();
+    }
+    @Override
+    public void run() {
+        while (true){
+            // possibility of creating a new colony
+            ArrayList<LycanthropeSolitary> listLycanSolitary = getAllLycanSolitary();
+            if (listLycanSolitary.size() > 1){
+                LycanthropeSolitary male = null;
+                LycanthropeSolitary female = null;
+                for (LycanthropeSolitary lycanthropeSolitary : listLycanSolitary){
+                    if (male == null && !lycanthropeSolitary.isSex()){
+                        male = lycanthropeSolitary;
+                    }
+                    if (female == null && lycanthropeSolitary.isSex()){
+                        female = lycanthropeSolitary;
+                    }
+                    if (female != null && male != null){
+                        Enclosure2 enclosureEmpty = giveEmptyEnclosure();
+                        if (enclosureEmpty == null) {
+                            enclosureEmpty = new Enclosure2();
+                            this.addEnclosure(enclosureEmpty);
+                            System.out.println("\u001B[34m" + "/\\ A new enclosure with a new lycan pack has just been created !! /\\");
+                        }
+                        else {
+                            System.out.println("\u001B[34m" + "/\\ An empty lycan pack just got two new alphas !! /\\");
+                        }
+                        male.setEnclosure(enclosureEmpty);
+                        female.setEnclosure(enclosureEmpty);
+                        male.goInPark();
+                        female.goInPark();
+                        break;
+                    }
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
     public void addEnclosure(Enclosure2 enclosure){listEnclosure.add(enclosure);}
+    public Enclosure2 giveEmptyEnclosure(){
+        for (Enclosure2 enclosure : listEnclosure){
+            if (enclosure.getLycanPack().getListLycan().isEmpty()){
+                return enclosure;
+            }
+        }
+        return null;
+    }
 
     public void showAllLycan(){
         System.out.println("\u001B[32m" + "ALL LYCANTHROPE IN COLONY : ");
         for (Enclosure2 enclosure : listEnclosure){
+            System.out.println("\u001B[33m" + "Enclosure n°" + enclosure.getID());
             for (LycanthropeInPack lycanthropeInPack : enclosure.getLycanPack().getListLycan()){
                 System.out.println(lycanthropeInPack);
             }
@@ -57,5 +121,19 @@ public class LycanthropeColony {
                 System.out.println(lycanthropeSolitary);
             }
         }
+    }
+    public void changeEnclosureLycanSolitary(LycanthropeSolitary lycanthropeSolitary, Enclosure2 newEnclosure){
+        Enclosure2 oldEnclosure = lycanthropeSolitary.getEnclosure();
+        oldEnclosure.removeLycanSolitary(lycanthropeSolitary);
+        lycanthropeSolitary.setEnclosure(newEnclosure);
+    }
+    public ArrayList<LycanthropeSolitary> getAllLycanSolitary(){
+        ArrayList<LycanthropeSolitary> lisLycanSolitary = new ArrayList<LycanthropeSolitary>();
+        for (Enclosure2 enclosure : listEnclosure){
+            for (LycanthropeSolitary lycanthropeSolitary : enclosure.getListSolitary()){
+                lisLycanSolitary.add(lycanthropeSolitary);
+            }
+        }
+        return lisLycanSolitary;
     }
 }
